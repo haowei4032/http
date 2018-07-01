127f8836b91e61de53348f16600bf28216bdbb70
haowei4032 <boss@haowei.me>
2018-07-02 07:07:51 +0800

    Add test case file

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use EastWood\Http\HttpRequest;
+
+$request = new HttpRequest();
+$request->setUrl('http://localhost/index.php');
+$request->setQueryData(['id' => 1, 'name' => 2]);
+echo $request->send();
